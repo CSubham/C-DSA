1079140154d2f627d279fe16e6615268ed2b5bb6
CSubham <csubham.dev@gmail.com>
2025-04-08 14:12:13 +0530

    Upto 70

--- Recursion/Sixtyseven.c
+++ Recursion/Sixtyseven.c
@@ -3,15 +3,23 @@
 double taylor(int x, int n);
 double pow1(int x, int n);
 double fact(int n);
+double taylor2(int x,int n, double s);
 
 int main(){
-    printf("%lf", taylor(4,10 ));
+    printf("%lf\n", taylor(2,10 ));
+    printf("%lf", taylor2(2,10,1 ));
     return 0;
 }
 
 double taylor(int x,int n){
     if(n==0) return 1.0;
     else return (double)(((pow1(x,n))/fact(n)) + taylor(x,n-1));
+}
+
+double taylor2(int x,int n, double s){
+    if(n==0) return s;
+    s = 1.0 + ((x/(double)n) *  s);
+    return  taylor2(x, n-1, s);
 }
 
 double pow1(int m, int n)
